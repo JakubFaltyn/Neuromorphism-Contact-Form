38f7099877d510d311f26b3c0a7e7140983eaefe
JakubFaltyn <jakubfaltyn@gmail.com>
2021-08-24 00:35:20 +0200

    .

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 
-
-$('.box img').on('click', function() {
-    $(this).toggleClass('clicked');
-  });
-  
+  $(".form__btn").click(function() {
+        $(".animation-container__plane").animate({opacity: 0}, 1);
+        $(".animation-container__plane").css("transform","translate(100px, -40px)");
+});
